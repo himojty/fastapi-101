7b4afb21528851830427ecee40f3e23406da15c9
ACER <himojty@gmail.com>
2025-06-02 19:12:03 +0300

    create user table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,10 +1,6 @@
-__all__ = (
-    "Base",
-    "DatabaseHelper",
-    "db_helper",
-    "Article",
-)
+__all__ = ("Base", "DatabaseHelper", "db_helper", "Article", "User")
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .article import Article
+from .user import User
--- core/models/user.py
+++ core/models/user.py
@@ -1,11 +1,11 @@
 from pydantic import EmailStr
-from sqlalchemy import Column
+from sqlalchemy import Column, String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
 
 
 class User(Base):
-    name: Mapped[str] = mapped_column(String(50), nullable=False)
+    name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[EmailStr] = mapped_column(String(255), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(512), nullable=False)
