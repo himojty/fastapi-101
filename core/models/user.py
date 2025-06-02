2160d8cfbaf13aa6b00dba904a48a8864a6ddba2
ACER <himojty@gmail.com>
2025-06-02 20:54:40 +0300

    create users model

--- core/models/user.py
+++ core/models/user.py
@@ -5,13 +5,17 @@ from sqlalchemy import Column, String
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from .article import Article
+    from .profile import Profile
 
 
-class User(Base):
+class User(UserRelationMixin, Base):
     name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[EmailStr] = mapped_column(String(255), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(512), nullable=False)
+
     articles: Mapped[list["Article"]] = relationship(back_populates="user")
+    profile: Mapped["Profile"] = relationship(back_populates="user")
