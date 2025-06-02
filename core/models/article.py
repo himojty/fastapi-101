7ca9c1beb0d1de3b7d5f0d51961896e8d6e9b376
ACER <himojty@gmail.com>
2025-06-02 20:57:11 +0300

    update article model

--- core/models/article.py
+++ core/models/article.py
@@ -1,22 +1,23 @@
 from typing import TYPE_CHECKING
 
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
+from .mixins import UserRelationMixin
 
 if TYPE_CHECKING:
     from .user import User
 
 
-class Article(Base):
+class Article(UserRelationMixin, Base):
+    # _user_id_nullable = False
+    # _user_id_unique = False
+    _user_back_populates = "posts"
+
     title: Mapped[str] = mapped_column(String(100), unique=True)
     body: Mapped[str] = mapped_column(
         Text,
         default="",
         server_default="",
     )
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-    )
-    user: Mapped["User"] = relationship(back_populates="articles")
