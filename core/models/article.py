0e995eb477d63b63cee6960096f68037f36430c3
ACER <himojty@gmail.com>
2025-06-02 19:35:56 +0300

    create relationships between user and article

--- core/models/article.py
+++ core/models/article.py
@@ -1,8 +1,22 @@
-from sqlalchemy.orm import Mapped
+from typing import TYPE_CHECKING
+
+from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
 
+if TYPE_CHECKING:
+    from .user import User
+
 
 class Article(Base):
-    title: Mapped[str]
-    content: Mapped[str]
+    title: Mapped[str] = mapped_column(String(100), unique=True)
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+    )
+    user: Mapped["User"] = relationship(back_populates="articles")
--- core/models/user.py
+++ core/models/user.py
@@ -1,11 +1,17 @@
+from typing import TYPE_CHECKING
+
 from pydantic import EmailStr
 from sqlalchemy import Column, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from .base import Base
+
+if TYPE_CHECKING:
+    from .article import Article
 
 
 class User(Base):
     name: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
     email: Mapped[EmailStr] = mapped_column(String(255), unique=True, nullable=False)
     password_hash: Mapped[str] = mapped_column(String(512), nullable=False)
+    articles: Mapped[list["Article"]] = relationship(back_populates="user")
