89f3b566db8980b354d2781d789cda7d8be4f4b3
himojty <mtryrd.13@gmail.com>
2025-06-24 15:29:24 +0300

    extend import

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,7 +1,8 @@
-__all__ = ("Base", "DatabaseHelper", "db_helper", "Article", "User", "Profile")
+__all__ = ("Base", "DatabaseHelper", "db_helper", "Article", "User", "Profile", "Item")
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
 from .article import Article
 from .user import User
 from .profile import Profile
+from .item import Item
