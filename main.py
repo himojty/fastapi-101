9dac1bf3b0ddfdf1f37b6a0a295a455ddf7db72e
himojty <mtryrd.13@gmail.com>
2025-06-28 23:18:35 +0300

    add CORS support

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 
 from core.config import settings
 from core.models import Base, db_helper
@@ -17,6 +18,17 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(router=api_v1_router, prefix="/api-v1")
+
+allow_origins = [
+    "http://localhost:63342",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allow_origins,  # ['*'] Разрешить все домены
+    allow_methods=["GET"],  # ['*'] Разрешить все методы (GET, POST и т.д.)
+    # allow_headers=["*"],  # Разрешить все заголовки
+)
 
 
 @app.get("/")
