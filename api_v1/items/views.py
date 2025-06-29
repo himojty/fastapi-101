e167c7126fc25636471688edfe17b02a43e6db51
himojty <mtryrd.13@gmail.com>
2025-06-29 15:36:57 +0300

    create routers for crud

--- api_v1/items/views.py
+++ api_v1/items/views.py
@@ -3,22 +3,18 @@ from typing import Annotated
 from fastapi import APIRouter, Path, status, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from core.models import db_helper
-from .schemas import ItemCreate, Item
 from . import crud
+from core.models import db_helper
+from .dependencies import item_by_id
+from .schemas import ItemCreate, Item
+
 
 router = APIRouter(prefix="/items", tags=["Items"])
 
 
 @router.get("/{item_id}")
-async def get_item(
-    item_id: Annotated[
-        int,
-        Path(..., gt=0, le=1000, description="Must be between 1 and 1000"),
-    ],
-    q: str | None = None,
-):
-    return {"item_id": item_id, "q": q}
+async def get_item(item: Item = Depends(item_by_id)):
+    return item
 
 
 @router.post(
@@ -26,8 +22,11 @@ async def get_item(
     status_code=status.HTTP_201_CREATED,
     response_model=ItemCreate,
 )
-async def create_item(item: ItemCreate):
-    return item
+async def create_item(
+    item: ItemCreate,
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+):
+    return await crud.create_item(session=session, item_in=item)
 
 
 @router.get("/")
@@ -35,3 +34,11 @@ async def get_items(
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     return await crud.get_items(session=session)
+
+
+@router.delete("/{item_id}/", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_item(
+    item: Item = Depends(item_by_id),
+    session: AsyncSession = Depends(db_helper.scoped_session_dependency),
+) -> None:
+    await crud.delete_item(session=session, item=item)
