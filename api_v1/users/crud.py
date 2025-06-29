16d8fb0ffdc1b2f7810ec9513bcbafcc69540f2c
ACER <himojty@gmail.com>
2025-06-29 22:45:18 +0300

    create part of crud for user and his profile

--- api_v1/users/crud.py
+++ api_v1/users/crud.py
@@ -1,11 +1,38 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-
-from core.models import db_helper, User, Profile, Article
+from sqlalchemy import select
+from core.models import User, Profile, Article
 
 
 async def create_user(session: AsyncSession, username: str) -> User:
     user = User(username=username)
     session.add(user)
     await session.commit()
-    print("user", user)
     return user
+
+
+async def get_user_by_username(session: AsyncSession, username: str) -> User | None:
+    stmt = select(User).where(User.username == username)
+    user = await session.scalar(stmt)
+    return user
+
+
+async def create_user_profile(
+    session: AsyncSession,
+    user_id: int,
+    first_name: str | None,
+    last_name: str | None,
+    bio: str | None = None,
+) -> Profile:
+    profile = Profile(
+        user_id=user_id, first_name=first_name, last_name=last_name, bio=bio
+    )
+    session.add(profile)
+    await session.commit()
+    return profile
+
+
+async def show_users_with_profiles(session: AsyncSession) -> list[User]:
+    stmt = select(User).order_by(User.id)
+    users = await session.scalars(stmt)
+    for user in users:
+        print(user)
