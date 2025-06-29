935df62b955560490a0bbb23745df538ae6a0bc5
himojty <mtryrd.13@gmail.com>
2025-06-29 15:36:33 +0300

    create crd (without update)

--- api_v1/items/crud.py
+++ api_v1/items/crud.py
@@ -1,10 +1,46 @@
+"""
+Create
+Read
+Update
+Delete
+"""
+
+from fastapi import Depends
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 from core.models import Item
+from .schemas import ItemCreate
 
 
-async def get_items(session: AsyncSession) -> list[Item] | None:
+async def get_items(
+    session: AsyncSession,
+) -> list[Item] | None:
     stmt = select(Item).order_by(Item.id)
     result = await session.execute(stmt)
     items = result.scalars().all()
     return list(items)
+
+
+async def get_item(
+    session: AsyncSession,
+    item_id: int,
+) -> Item | None:
+    return await session.get(Item, item_id)
+
+
+async def create_item(
+    session: AsyncSession,
+    item_in: ItemCreate,
+) -> Item:
+    item = Item(**item_in.model_dump())
+    session.add(item)
+    await session.commit()
+    return item
+
+
+async def delete_item(
+    session: AsyncSession,
+    item: Item,
+) -> None:
+    await session.delete(item)
+    await session.commit()
