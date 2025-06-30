a81ddc348577555af5a6b9927747b4b6dfb71d67
himojty <mtryrd.13@gmail.com>
2025-06-30 14:36:36 +0300

    add router for demo auth

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -2,6 +2,7 @@ __all__ = ("router",)
 from .articles.views import router as articles_router
 from .users.views import router as users_router
 from .items.views import router as items_router
+from .demo_auth.views import router as demo_auth_router
 
 from fastapi import APIRouter
 
@@ -9,3 +10,4 @@ router = APIRouter()
 router.include_router(router=users_router)
 router.include_router(router=items_router)
 router.include_router(router=articles_router)
+router.include_router(router=demo_auth_router)
