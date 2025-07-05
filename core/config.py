29f0c09ea88b9a85f98b9632766517a09ea5f9e8
himojty <mtryrd.13@gmail.com>
2025-07-05 18:06:23 +0300

    add CORSettings

--- core/config.py
+++ core/config.py
@@ -7,6 +7,12 @@ BASE_DIR = Path(__file__).parent.parent
 DB_PATH = BASE_DIR / "db.sqlite3"
 
 
+class CORSettings(BaseModel):
+    allow_origins: list[str] = [
+        "http://localhost:63342",
+    ]
+
+
 class DbSettings(BaseModel):
     url: str = f"sqlite+aiosqlite:///{DB_PATH}"
     echo: bool = False
@@ -16,6 +22,7 @@ class Settings(BaseSettings):
     api_v1_prefix: str = "/api/v1"
 
     db: DbSettings = DbSettings()
+    cors: CORSettings = CORSettings()
 
 
 settings = Settings()
