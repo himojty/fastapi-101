5b69e748253fff9dc2ee04984f1d959b76ca1bbb
himojty <mtryrd.13@gmail.com>
2025-07-17 14:33:41 +0300

    change ip in links

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ app.add_middleware(
 @app.get("/", response_class=HTMLResponse)
 def home():
     return """
-    <a href="http://127.0.0.1:8000/docs">Documentation</a><br>
-    <a href="http://127.0.0.1:8000/redoc">ReDoc</a>
+    <a href="http://0.0.0.0:8000/docs">Documentation</a><br>
+    <a href="http://0.0.0.0:8000/redoc">ReDoc</a>
     """
 
 
